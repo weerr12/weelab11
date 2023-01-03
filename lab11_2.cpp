16efdfb3823810190f3d97517e5e0f9d30b9c9dd
werasak mayer <jokerwwe0542@gmail.com>
2023-01-03 15:51:58 +0700

    "lab11_2"

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -6,12 +6,13 @@ using namespace std;
 int main (){
 	ifstream source;
 	ofstream dest;
-	source.open("cheerbook.txt") ;
+	source.open("cheerbook.txt");
 	dest.open("cheerbook_copy.txt");
-	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
-
+	cout <<"-------------------- BOOM ---------------------"<<endl;
+	string textline;
+	while(getline(source,textline))
+		cout << textline <<endl;
+	cout << "-------------------- HA!! ---------------------";
     source.close();
     dest.close();
 	return 0;
--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,24 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cstdlib>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int count=0;
+    float sum=0,_sqrt;
+    string textline;
+    ifstream score("score.txt");
+    while (getline(score,textline))
+    {
+        sum+=atof(textline.c_str());
+        _sqrt+=atof(textline.c_str())*atof(textline.c_str());
+        count++;
+    }
+    cout << "Number of data = "<<count<<"\n";
+    cout << setprecision(3);
+    cout << "Mean = "<<sum/count<<"\n";
+    cout << "Standard deviation = "<<sqrt(_sqrt/count-pow(sum/count,2));
+    score.close();
+}
